c753d090b590127717f91bed82fdb982cb6fb4af
thanks4opensource <markrubn@yahoo.com>
2020-01-20 00:11:54 -0800

    copyright repo name, duplicate files

--- examples/blue_pill/usb_mcu_init.cxx
+++ examples/blue_pill/usb_mcu_init.cxx
@@ -3,18 +3,18 @@
 //
 // This file is part of papoon_usb.
 //
-// The regbits_stm program is free software: you can redistribute it
+// The papoon_usb program is free software: you can redistribute it
 // and/or modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation, either version 3 of
 // the License, or (at your option) any later version.
 //
-// The regbits_stm program is distributed in the hope that it will be
+// The papoon_usb program is distributed in the hope that it will be
 // useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
 // General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// (LICENSE.txt) along with the regbits_stm program.  If not, see
+// (LICENSE.txt) along with the papoon_usb program.  If not, see
 // <https://www.gnu.org/licenses/gpl.html>
 
 
